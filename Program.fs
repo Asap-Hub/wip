60530eccb0aa0d734da6d40139d1e638f63a4d9c
Abdallah Mustapha <abdullahmustapha59@gmail.com>
2023-05-12 09:44:32 +0000

    base

---
+++ Program.fs
@@ -0,0 +1,37 @@
+﻿open System
+
+let  promptUser() =
+    Console.WriteLine("(d)eposit, (w)ithdraw, e(x)ist:")
+    Console.ReadLine()
+
+let getAmount() =
+    Console.WriteLine("Enter Amount to withdraw")
+    Console.ReadLine() |> Decimal.Parse
+
+[<EntryPoint>]
+let main argv=
+    Console.WriteLine("Hello from the transaction processor!")
+
+    let mutable balance:decimal = 0m
+
+
+    let mutable running = true
+    printfn "Balance %A" balance
+    while running do
+      
+
+        let action= promptUser()
+        let inputAmount = getAmount()
+        printfn "Available Balance is:  %A" balance
+
+        balance <- 
+        match action with 
+        | "d" -> balance + inputAmount 
+        | "w" -> balance - inputAmount 
+        | _ -> 
+            running <- action <> "x"
+            balance 
+
+    Console.WriteLine("Bye!")
+    0
+
